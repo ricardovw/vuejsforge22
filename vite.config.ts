79e2c6d82e7eba1ee26c35c283337f80034805dd
Ricardo <rvw_po@mac.com>
2022-07-13 17:27:07 +0200

    ex: 0

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "~": fileURLToPath(new URL("./src/assets", import.meta.url)),
     },
   },
 });
